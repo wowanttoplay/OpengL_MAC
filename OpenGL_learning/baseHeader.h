6eead81571879d02c357da2d189d84b9a4b0d1c8
virgil <virgiljfan.tencent.com>
2020-09-20 23:33:11 +0800

    基本环境，显示三角形

---
+++ OpenGL_learning/baseHeader.h
@@ -0,0 +1,24 @@
+//
+//  baseHeader.h
+//  OpenGL_learning
+//
+//  Created by virgil on 2020/9/12.
+//  Copyright © 2020 virgil. All rights reserved.
+//
+
+#ifndef baseHeader_h
+#define baseHeader_h
+
+
+#include <GLTools.h>
+#include <GLShaderManager.h>
+#include <math3d.h>
+
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+#endif /* baseHeader_h */
