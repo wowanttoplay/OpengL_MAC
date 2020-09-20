5aa3ab004f3e5e74847b257a4f875c5fd7bae37e
virgil <virgiljfan.tencent.com>
2020-09-20 23:43:00 +0800

    修正格式

--- OpenGL_learning/main.cpp
+++ OpenGL_learning/main.cpp
@@ -18,53 +18,53 @@ GLShaderManager    shaderManager;
 // Window has changed size, or has just been created. In either case, we need
 // to use the window dimensions to set the viewport and the projection matrix.
 void ChangeSize(int w, int h)
-    {
+{
     glViewport(0, 0, w, h);
-    }
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // This function does any needed initialization on the rendering context.
 // This is the first opportunity to do any OpenGL related tasks.
 void SetupRC()
-    {
+{
     // Blue background
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f );
     
     shaderManager.InitializeStockShaders();
-
+    
     // Load up a triangle
     GLfloat vVerts[] = { -0.5f, 0.0f, 0.0f,
-                          0.5f, 0.0f, 0.0f,
-                          0.0f, 0.5f, 0.0f };
-
+        0.5f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f };
+    
     triangleBatch.Begin(GL_TRIANGLES, 3);
     triangleBatch.CopyVertexData3f(vVerts);
     triangleBatch.End();
-    }
+}
 
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // Called to draw scene
 void RenderScene(void)
-    {
+{
     // Clear the window with current clearing color
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
+    
     GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
     shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
     triangleBatch.Draw();
-
+    
     // Perform the buffer swap to display back buffer
     glutSwapBuffers();
-    }
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // Main entry point for GLUT based programs
 int main(int argc, char* argv[])
-    {
+{
     gltSetWorkingDirectory(argv[0]);
     
     glutInit(&argc, argv);
@@ -73,15 +73,15 @@ int main(int argc, char* argv[])
     glutCreateWindow("Triangle");
     glutReshapeFunc(ChangeSize);
     glutDisplayFunc(RenderScene);
-
+    
     GLenum err = glewInit();
     if (GLEW_OK != err) {
         fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
         return 1;
-        }
+    }
     
     SetupRC();
-
+    
     glutMainLoop();
     return 0;
-    }
+}
