f822b7abefbfeac18958f621f30a7b971b2cdba5
virgil <virgiljfan.tencet.com>
2020-10-12 23:39:47 +0800

    glew , glfw, gltools

--- OpenGL_learning/main.cpp
+++ OpenGL_learning/main.cpp
@@ -1,9 +1,14 @@
-// Move.cpp
-// Move a Block based on arrow key movements
-
-#include <GLTools.h>    // OpenGL toolkit
+// Tunnel.cpp
+// Demonstrates mipmapping and using texture objects
+// OpenGL SuperBible
+// Richard S. Wright Jr.
+#include <GLTools.h>
 #include <GLShaderManager.h>
-#include <math3d.h>
+#include <GLFrustum.h>
+#include <GLBatch.h>
+#include <GLFrame.h>
+#include <GLMatrixStack.h>
+#include <GLGeometryTransform.h>
 
 #ifdef __APPLE__
 #include <glut/glut.h>
@@ -12,134 +17,309 @@
 #include <GL/glut.h>
 #endif
 
-GLBatch    squareBatch;
-GLShaderManager    shaderManager;
 
+GLShaderManager        shaderManager;            // Shader Manager
+GLMatrixStack        modelViewMatrix;        // Modelview Matrix
+GLMatrixStack        projectionMatrix;        // Projection Matrix
+GLFrustum            viewFrustum;            // View Frustum
+GLGeometryTransform    transformPipeline;        // Geometry Transform Pipeline
 
-GLfloat blockSize = 0.1f;
-GLfloat vVerts[] = { -blockSize, -blockSize, 0.0f,
-                      blockSize, -blockSize, 0.0f,
-                      blockSize,  blockSize, 0.0f,
-                     -blockSize,  blockSize, 0.0f};
+GLBatch             floorBatch;
+GLBatch             ceilingBatch;
+GLBatch             leftWallBatch;
+GLBatch             rightWallBatch;
 
-GLfloat xPos = 0.0f;
-GLfloat yPos = 0.0f;
+GLfloat             viewZ = -65.0f;
+
+// Texture objects
+#define TEXTURE_BRICK   0
+#define TEXTURE_FLOOR   1
+#define TEXTURE_CEILING 2
+#define TEXTURE_COUNT   3
+GLuint  textures[TEXTURE_COUNT];
+const char *szTextureFiles[TEXTURE_COUNT] = { "brick.tga", "floor.tga", "ceiling.tga" };
+
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// This function does any needed initialization on the rendering context.
-// This is the first opportunity to do any OpenGL related tasks.
-void SetupRC()
+// Change texture filter for each texture object
+void ProcessMenu(int value)
+{
+    GLfloat fLargest;
+    GLint iLoop;
+    
+    for(iLoop = 0; iLoop < TEXTURE_COUNT; iLoop++)
     {
+        glBindTexture(GL_TEXTURE_2D, textures[iLoop]);
+        
+        switch(value)
+        {
+            case 0:
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                break;
+                
+            case 1:
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                break;
+                
+            case 2:
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                break;
+                
+            case 3:
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+                break;
+                
+            case 4:
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+                break;
+                
+            case 5:
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+                break;
+                
+            case 6:
+                glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &fLargest);
+                glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, fLargest);
+                break;
+                
+            case 7:
+                glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 1.0f);
+                break;
+        }
+    }
+    
+    // Trigger Redraw
+    glutPostRedisplay();
+}
+
+
+//////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering
+// context.  Here it sets up and initializes the texture objects.
+void SetupRC()
+{
+    GLbyte *pBytes;
+    GLint iWidth, iHeight, iComponents;
+    GLenum eFormat;
+    GLint iLoop;
+    
     // Black background
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f );
+    glClearColor(0.0f, 0.0f, 0.0f,1.0f);
     
     shaderManager.InitializeStockShaders();
-
-    // Load up a triangle
-    squareBatch.Begin(GL_TRIANGLE_FAN, 4);
-    squareBatch.CopyVertexData3f(vVerts);
-    squareBatch.End();
-    }
-
-// Respond to arrow keys by moving the camera frame of reference
-void SpecialKeys(int key, int x, int y)
+    
+    // Load textures
+    glGenTextures(TEXTURE_COUNT, textures);
+    for(iLoop = 0; iLoop < TEXTURE_COUNT; iLoop++)
     {
-    GLfloat stepSize = 0.025f;
-
-
-    if(key == GLUT_KEY_UP)
-        yPos += stepSize;
-
-    if(key == GLUT_KEY_DOWN)
-        yPos -= stepSize;
-    
-    if(key == GLUT_KEY_LEFT)
-        xPos -= stepSize;
-
-    if(key == GLUT_KEY_RIGHT)
-        xPos += stepSize;
-
-    // Collision detection
-    if(xPos < (-1.0f + blockSize)) xPos = -1.0f + blockSize;
-    
-    if(xPos > (1.0f - blockSize)) xPos = 1.0f - blockSize;
-    
-    if(yPos < (-1.0f + blockSize))  yPos = -1.0f + blockSize;
-    
-    if(yPos > (1.0f - blockSize)) yPos = 1.0f - blockSize;
-
-    glutPostRedisplay();
+        // Bind to next texture object
+        glBindTexture(GL_TEXTURE_2D, textures[iLoop]);
+        
+        // Load texture, set filter and wrap modes
+        pBytes = gltReadTGABits(szTextureFiles[iLoop],&iWidth, &iHeight,
+                                &iComponents, &eFormat);
+        
+        // Load texture, set filter and wrap modes
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexImage2D(GL_TEXTURE_2D, 0, iComponents, iWidth, iHeight, 0, eFormat, GL_UNSIGNED_BYTE, pBytes);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        // Don't need original texture data any more
+        free(pBytes);
     }
+    
+    // Build Geometry
+    GLfloat z;
+    floorBatch.Begin(GL_TRIANGLE_STRIP, 28, 1);
+    for(z = 60.0f; z >= 0.0f; z -=10.0f)
+    {
+        floorBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+        floorBatch.Vertex3f(-10.0f, -10.0f, z);
+        
+        floorBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+        floorBatch.Vertex3f(10.0f, -10.0f, z);
+        
+        floorBatch.MultiTexCoord2f(0, 0.0f, 1.0f);
+        floorBatch.Vertex3f(-10.0f, -10.0f, z - 10.0f);
+        
+        floorBatch.MultiTexCoord2f(0, 1.0f, 1.0f);
+        floorBatch.Vertex3f(10.0f, -10.0f, z - 10.0f);
+    }
+    floorBatch.End();
+    
+    ceilingBatch.Begin(GL_TRIANGLE_STRIP, 28, 1);
+    for(z = 60.0f; z >= 0.0f; z -=10.0f)
+    {
+        ceilingBatch.MultiTexCoord2f(0, 0.0f, 1.0f);
+        ceilingBatch.Vertex3f(-10.0f, 10.0f, z - 10.0f);
+        
+        ceilingBatch.MultiTexCoord2f(0, 1.0f, 1.0f);
+        ceilingBatch.Vertex3f(10.0f, 10.0f, z - 10.0f);
+        
+        ceilingBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+        ceilingBatch.Vertex3f(-10.0f, 10.0f, z);
+        
+        ceilingBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+        ceilingBatch.Vertex3f(10.0f, 10.0f, z);
+    }
+    ceilingBatch.End();
+    
+    leftWallBatch.Begin(GL_TRIANGLE_STRIP, 28, 1);
+    for(z = 60.0f; z >= 0.0f; z -=10.0f)
+    {
+        leftWallBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+        leftWallBatch.Vertex3f(-10.0f, -10.0f, z);
+        
+        leftWallBatch.MultiTexCoord2f(0, 0.0f, 1.0f);
+        leftWallBatch.Vertex3f(-10.0f, 10.0f, z);
+        
+        leftWallBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+        leftWallBatch.Vertex3f(-10.0f, -10.0f, z - 10.0f);
+        
+        leftWallBatch.MultiTexCoord2f(0, 1.0f, 1.0f);
+        leftWallBatch.Vertex3f(-10.0f, 10.0f, z - 10.0f);
+    }
+    leftWallBatch.End();
+    
+    
+    rightWallBatch.Begin(GL_TRIANGLE_STRIP, 28, 1);
+    for(z = 60.0f; z >= 0.0f; z -=10.0f)
+    {
+        rightWallBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+        rightWallBatch.Vertex3f(10.0f, -10.0f, z);
+        
+        rightWallBatch.MultiTexCoord2f(0, 0.0f, 1.0f);
+        rightWallBatch.Vertex3f(10.0f, 10.0f, z);
+        
+        rightWallBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+        rightWallBatch.Vertex3f(10.0f, -10.0f, z - 10.0f);
+        
+        rightWallBatch.MultiTexCoord2f(0, 1.0f, 1.0f);
+        rightWallBatch.Vertex3f(10.0f, 10.0f, z - 10.0f);
+    }
+    rightWallBatch.End();
+}
+
+///////////////////////////////////////////////////
+// Shutdown the rendering context. Just deletes the
+// texture objects
+void ShutdownRC(void)
+{
+    glDeleteTextures(TEXTURE_COUNT, textures);
+}
 
 
+///////////////////////////////////////////////////
+// Respond to arrow keys, move the viewpoint back
+// and forth
+void SpecialKeys(int key, int x, int y)
+{
+    if(key == GLUT_KEY_UP)
+        viewZ += 0.5f;
+    
+    if(key == GLUT_KEY_DOWN)
+        viewZ -= 0.5f;
+    
+    // Refresh the Window
+    glutPostRedisplay();
+}
 
+/////////////////////////////////////////////////////////////////////
+// Change viewing volume and viewport.  Called when window is resized
+void ChangeSize(int w, int h)
+{
+    GLfloat fAspect;
+    
+    // Prevent a divide by zero
+    if(h == 0)
+        h = 1;
+    
+    // Set Viewport to window dimensions
+    glViewport(0, 0, w * 2, h * 2);
+    
+    fAspect = (GLfloat)w/(GLfloat)h;
+    
+    // Produce the perspective projection
+    viewFrustum.SetPerspective(80.0f,fAspect,1.0,120.0);
+    projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
+    transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
+    
+}
 
-
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 // Called to draw scene
 void RenderScene(void)
-    {
+{
     // Clear the window with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-    GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+    glClear(GL_COLOR_BUFFER_BIT);
     
-    M3DMatrix44f mFinalTransform, mTranslationMatrix, mRotationMatrix;
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.Translate(0.0f, 0.0f, viewZ);
     
-    // Just Translate
-    m3dTranslationMatrix44(mTranslationMatrix, xPos, yPos, 0.0f);
+    shaderManager.UseStockShader(GLT_SHADER_TEXTURE_REPLACE, transformPipeline.GetModelViewProjectionMatrix(), 0);
     
-    // Rotate 5 degrees evertyime we redraw
-    static float yRot = 0.0f;
-    yRot += 5.0f;
-    m3dRotationMatrix44(mRotationMatrix, m3dDegToRad(yRot), 0.0f, 0.0f, 1.0f);
+    glBindTexture(GL_TEXTURE_2D, textures[TEXTURE_FLOOR]);
+    floorBatch.Draw();
     
-    m3dMatrixMultiply44(mFinalTransform, mTranslationMatrix, mRotationMatrix);
+    glBindTexture(GL_TEXTURE_2D, textures[TEXTURE_CEILING]);
+    ceilingBatch.Draw();
     
+    glBindTexture(GL_TEXTURE_2D, textures[TEXTURE_BRICK]);
+    leftWallBatch.Draw();
+    rightWallBatch.Draw();
     
-    shaderManager.UseStockShader(GLT_SHADER_FLAT, mFinalTransform, vRed);
-    squareBatch.Draw();
-
-    // Perform the buffer swap
+    modelViewMatrix.PopMatrix();
+    
+    // Buffer swap
     glutSwapBuffers();
-    }
+}
 
 
-
-///////////////////////////////////////////////////////////////////////////////
-// Window has changed size, or has just been created. In either case, we need
-// to use the window dimensions to set the viewport and the projection matrix.
-void ChangeSize(int w, int h)
-    {
-    glViewport(0, 0, w, h);
-    }
-
-///////////////////////////////////////////////////////////////////////////////
-// Main entry point for GLUT based programs
-int main(int argc, char* argv[])
-    {
+//////////////////////////////////////////////////////
+// Program entry point
+int main(int argc, char *argv[])
+{
     gltSetWorkingDirectory(argv[0]);
     
+    // Standard initialization stuff
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
-    glutCreateWindow("Move Block with Arrow Keys");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Anisotropic Tunnel");
+    glutReshapeFunc(ChangeSize);
+    glutSpecialFunc(SpecialKeys);
+    glutDisplayFunc(RenderScene);
+    
+    // Add menu entries to change filter
+    glutCreateMenu(ProcessMenu);
+    glutAddMenuEntry("GL_NEAREST",0);
+    glutAddMenuEntry("GL_LINEAR",1);
+    glutAddMenuEntry("GL_NEAREST_MIPMAP_NEAREST",2);
+    glutAddMenuEntry("GL_NEAREST_MIPMAP_LINEAR", 3);
+    glutAddMenuEntry("GL_LINEAR_MIPMAP_NEAREST", 4);
+    glutAddMenuEntry("GL_LINEAR_MIPMAP_LINEAR", 5);
+    glutAddMenuEntry("Anisotropic Filter", 6);
+    glutAddMenuEntry("Anisotropic Off", 7);
+    
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
     
     GLenum err = glewInit();
-    if (GLEW_OK != err)
-        {
-        // Problem: glewInit failed, something is seriously wrong.
-        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
         return 1;
-        }
-    
-    glutReshapeFunc(ChangeSize);
-    glutDisplayFunc(RenderScene);
-    glutSpecialFunc(SpecialKeys);
-
-    SetupRC();
-
-    glutMainLoop();
-    return 0;
     }
+    
+    // Startup, loop, shutdown
+    SetupRC();
+    glutMainLoop();
+    ShutdownRC();
+    
+    return 0;
+}
+
+
+
